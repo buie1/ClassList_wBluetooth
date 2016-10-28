bf7933fe3cf9cbadce3cf147eadc83b4cb4ce9ac
pjm41@duke.edu <pjm41@duke.edu>
2016-10-27 21:17:10 -0400

    fixed photo issue and storyboard placement

--- jypc-ece590-hw6/SendBlueToothViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  SendBlueToothViewController.swift
-//  jypc-ece590-hw6
-//
-//  Created by Peter Murphy on 10/27/16.
-//  Copyright © 2016 Jonathan Buie. All rights reserved.
-//
-
-import Foundation
